306bee11042e6781662a3b6747efbc03c6c0711a
michaelbi <jianhua.bi@hotmail.com>
2022-12-03 15:58:58 +0800

    final rel for tap1.3 frontend requirehttps false

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ export const environment = {
   baseHref: '/frontend/',
   authConfig: {
     issuer: 'http://authserver-1.dev-space.appsso.tap.h2o-4-3220.h2o.vmware.com',
-    clientId: 'dev-space_client-registration'
+    clientId: 'dev-space_client-registration',
+    requireHttps: false
   },
   endpoints: {
     orders: window.location.origin + '/services/order-service/api/v1/orders',
