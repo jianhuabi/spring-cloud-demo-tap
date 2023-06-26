f5721e1577ea62beb4b79867fd277ec553a8b23a
michaelbi <jianhua.bi@hotmail.com>
2023-06-26 20:27:51 +0800

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   baseHref: '/frontend/',
   authConfig: {
-    issuer: 'http://authserver-1.dev-space.appsso.tap.h2o-4-3220.h2o.vmware.com',
+    issuer: 'http://authserver-1.dev-space.appsso.tap.h2o-4-13952.h2o.vmware.com',
     clientId: 'dev-space_client-registration',
     requireHttps: false
   },
